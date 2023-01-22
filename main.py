010b099badf2ae89bd2db106234844c3ad3f530e
Geovana Martins <50850895+geovanams@users.noreply.github.com>
2023-01-22 03:39:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 import sys
 
-r=requests.get("https://api.github.com/repos/"+sys.argv[1]+"/"+sys.argv[2]+"/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
+r=requests.get("https://api.github.com/repos/"+sys.argv[1]+"/"+sys.argv[2]+"/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
 
 obj = (r.text)
 objeto = json.loads(obj)
