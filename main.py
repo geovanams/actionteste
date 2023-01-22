93f554143bf2175cae075c2e9f3d9e18e6084394
Geovana Martins <50850895+geovanams@users.noreply.github.com>
2023-01-22 02:02:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-import requests
-import json
-#import argparse
-import os
+import sys
 
-#parser = argparse.ArgumentParser(description="List Branches")
-#parser.add_argument('--repos', help='This is parameter1', required=True)
-#parser.add_argument('--owner', help='This is parameter1', required=True)
-
-
-# r=requests.get("https://api.github.com/repos/"+parser.parse_args().owner+"/"+parser.parse_args().repos+"/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
-
-#r=requests.get("https://api.github.com/repos/geovanams/hello-world-docker-action/branches", headers={"Accept": "application/vnd.github+json" ,"X-GitHub-Api-Version":"2022-11-28", "Authorization": "Bearer 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"})
-
-# print(r.request.path_url)
-
-# obj = (r.text)
-# objeto = json.loads(obj)
-
-# json_formatted_str = json.dumps(objeto, indent=2)
-# print(json_formatted_str)
-
-# for v in objeto:
-#     print(v['name'])
-
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--name', type=str, help='Name of the user')
-args = parser.parse_args()
-
-print(f'Hello, {args.name}!')
+print("hello " + sys.argv[1])
