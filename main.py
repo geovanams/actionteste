9af51a7f9381d9f519055af5fecd2a734b359c94
Geovana Martins <50850895+geovanams@users.noreply.github.com>
2023-01-22 03:44:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ objeto = json.loads(obj)
 print("\n"+sys.argv[2]+ " repo branches:")
 for v in objeto:
     print(v)
-    print(v['name'])
 
 print("\nBranches Details:")
 json_formatted_str = json.dumps(objeto, indent=2)
